ab92a4635e48094f093d5caf17d6daef9fa23dbd
DraculaKris <125392343+DraculaKris@users.noreply.github.com>
2023-03-21 15:36:14 -0300

    Add files via upload

---
+++ pythonpdf/programação/domino.py
@@ -0,0 +1,2 @@
+num = int(input("Digite N: "))
+print ((num+1)*(num+2)/ 2)
---
+++ pythonpdf/programação/placa.py
@@ -0,0 +1,14 @@
+x = int(input("Insira o número x: "))
+Y = int(input("Insira o número y: "))
+M = int(input("Insira o número x: "))
+for x in range (0,M):
+    xi = int(input("\n Qual a largura do cliente? "))
+    yi = int(input("\n Qual a altura do cliente? "))
+    
+    if xi > xi:
+        print("Não")
+    elif yi > Y:
+        print ("Não")
+    else:
+        print ("Sim")
+        
